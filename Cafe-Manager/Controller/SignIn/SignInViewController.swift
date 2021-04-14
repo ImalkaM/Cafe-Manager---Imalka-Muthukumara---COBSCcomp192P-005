ae75c90a40f6f59d8de64921e4cf7a1069cec2c4
Imalka <Imalka.emedia@gmail.com>
2021-04-14 12:40:42 +0530

    Added login to home

--- Cafe-Manager/Controller/SignIn/SignInViewController.swift
+++ Cafe-Manager/Controller/SignIn/SignInViewController.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import Loaf
+import Firebase
 
 class SignInViewController: UIViewController {
     
+    var ref: DatabaseReference!
     
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
@@ -16,11 +19,67 @@ class SignInViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        ref = Database.database().reference()
         setupCustomUI()
     }
     
     @IBAction func loginTapped(_ sender: UIButton) {
+        
+        if !FieldValidator.isValidEmail(emailField.text ?? ""){
+            Loaf("Invalid email address", state: .error, sender: self).show()
+            return
+        }
+        
+        
+        if !FieldValidator.isValidPassword(pass: passwordField.text ?? "", minLength: 8, maxLength: 20){
+            
+            Loaf("Invalid password", state: .error, sender: self).show()
+            return
+            
+        }
+        
+        auhtenticateUser(email: emailField.text!, password: passwordField.text!)
+    }
+    func auhtenticateUser(email:String,password:String){
+        
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let err =  error{
+                Loaf("\(err.localizedDescription)", state: .error, sender: self).show()
+            }
+            else{
+                if let email = authResult?.user.email{
+                    self.getUserData(email: email)
+                }else{
+                    Loaf("User not found!", state: .error, sender: self).show()
+                }
+                self.performSegue(withIdentifier: K.loginToHomeSeauge, sender: self)
+                
+            }
+        }
+    }
+    func getUserData(email:String){
+        
+        ref.child("users")
+            .child(email .replacingOccurrences(of: "@", with: "_")
+                    .replacingOccurrences(of: ".", with: "_")).observe(.value, with: {(snapshot) in
+                        if snapshot.hasChildren(){
+                            if let data = snapshot.value{
+                                
+                                if let userData = data as? [String:String]{
+                                    let user = User(name: userData["userName"]!,
+                                                    email: userData["userEmail"]!,
+                                                    password: userData["userPassword"]!,
+                                                    phonenumber: userData["userPhone"]!)
+                                    
+                                    
+                                    let sessionManager = SessionManager()
+                                    sessionManager.saveUserLogin(user: user)
+                                }
+                            }
+                        }else{
+                            Loaf("User not found!", state: .error, sender: self).show()
+                        }
+                    })
     }
     
     func setupCustomUI(){
@@ -28,4 +87,6 @@ class SignInViewController: UIViewController {
         CustomUI.setupTextField(txtField: passwordField)
         CustomUI.setupAuthButton(btnCustom: loginButton)
     }
+    
 }
+
