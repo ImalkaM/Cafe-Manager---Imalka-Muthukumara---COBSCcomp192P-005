74b4177bf981058f20a0b76329d746680e3a577a
Imalka <Imalka.emedia@gmail.com>
2021-04-14 22:05:18 +0530

    Added tab bar assests

--- Cafe-Manager/Controller/Store /StoreViewController.swift
+++ Cafe-Manager/Controller/Store /StoreViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class StoreViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var previewBtn: UIButton!
+    @IBOutlet weak var menuBtn: UIButton!
+    @IBOutlet weak var categoryBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupCustomUI()
     }
     
-
+    func setupCustomUI(){
+        CustomUI.setupAuthButton(btnCustom: previewBtn)
+        CustomUI.setupAuthButton(btnCustom: menuBtn)
+        CustomUI.setupAuthButton(btnCustom: categoryBtn)
+    }
 
 }
