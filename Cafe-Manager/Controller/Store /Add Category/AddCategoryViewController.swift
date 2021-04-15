becc50056f9cf6e662aeb835803956a44a778dad
Imalka <Imalka.emedia@gmail.com>
2021-04-15 15:23:30 +0530

    Implemented swipe to delete category and all the validations.

--- Cafe-Manager/Controller/Store /Add Category/AddCategoryViewController.swift
+++ Cafe-Manager/Controller/Store /Add Category/AddCategoryViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import Firebase
+import Loaf
 
 class AddCategoryViewController: UIViewController {
+    var ref: DatabaseReference!
+    var category:Category = Category(categoryID: "", categoryName: "")
+    var categoryCollection:[Category] = []
     
-    var category:[Category] = []
-
     @IBOutlet weak var categoryTextField: UITextField!
     @IBOutlet weak var addButton: UIButton!
     
@@ -18,13 +21,21 @@ class AddCategoryViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = Database.database().reference()
         setupCustomUI()
         categoryTable.register(UINib(nibName: K.category.nibNameCategoryTable, bundle: nil), forCellReuseIdentifier: K.category.categoryTableCell)
+        getCategorys()
     }
-
+    
     @IBAction func addButtonTapped(_ sender: UIButton) {
-        category.append(Category(categoryID: "1", categoryName: categoryTextField.text ?? ""))
-        categoryTable.reloadData()
+        if let catName = categoryTextField.text{
+            if catName.isEmpty{
+                Loaf("Category name cannot be empty.", state: .error, sender: self).show()
+            }else{
+                category.categoryName = catName
+                createCategory()
+            }
+        }
     }
     
     @IBAction func backBtnTapped(_ sender: UIButton) {
@@ -34,17 +45,63 @@ class AddCategoryViewController: UIViewController {
         CustomUI.setupTextField(txtField: categoryTextField)
         CustomUI.setupAuthButton(btnCustom: addButton)
     }
+    
+    func createCategory(){
+        let categoryData = [
+            "categoryName" : category.categoryName
+        ]
+        self.ref.child("category")
+            .childByAutoId()
+            .setValue(categoryData){ (error, ref) in
+                if let err =  error{
+                    Loaf("\(err.localizedDescription)", state: .error, sender: self).show()
+                }
+                else{
+                    self.categoryTable.reloadData()
+                    Loaf("Category added successfully", state: .success, sender: self).show()
+                    self.dismiss(animated: true, completion: nil)
+                }
+            }
+    }
+    
+    func getCategorys(){
+        
+        self.ref.child("category").observe(.value) { (snapshot) in
+            if let data = snapshot.value{
+                if let orders = data as? [String:Any]{
+                    self.categoryCollection.removeAll()
+                    for singleCategory in orders{
+                        if let categoryInfo = singleCategory.value as? [String:Any]{
+                            self.category.categoryName = categoryInfo["categoryName"] as! String
+                            self.category.categoryID = singleCategory.key
+                            self.categoryCollection.append(self.category)
+                        }
+                    }
+                    self.categoryTable.reloadData()
+                }
+            }
+        }
+    }
+    
+    func deleteCategory(index:Int){
+        
+        self.ref.child("category").child(categoryCollection[index].categoryID).removeValue { (err, ref) in
+            if err != nil{
+                
+            }
+        }
+    }
 }
 
 extension AddCategoryViewController:UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return category.count
+        return categoryCollection.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = categoryTable.dequeueReusableCell(withIdentifier: K.category.categoryTableCell, for: indexPath) as! CategoryTableViewCell
         
-        cell.setupUI(category: category[indexPath.row])
+        cell.setupUI(category: categoryCollection[indexPath.row])
         //cell.setupUI(order: orders[indexPath.row])
         
         return cell
@@ -55,10 +112,14 @@ extension AddCategoryViewController:UITableViewDelegate{
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            category.remove(at: indexPath.row)
-               tableView.deleteRows(at: [indexPath], with: .fade)
-           } else if editingStyle == .insert {
-               // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-           }
+            deleteCategory(index: indexPath.row)
+            categoryCollection.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            categoryCollection.removeAll()
+            getCategorys()
+            
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
     }
 }
